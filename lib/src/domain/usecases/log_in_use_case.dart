20a4b007824e44d576e7b39f71ae8530aceb286f
peteragurto <130622718+peteragurto@users.noreply.github.com>
2024-05-01 23:53:34 -0500

    Nueva implementación en Auth, de Caso de uso y proveedor.

---
+++ lib/src/domain/usecases/log_in_use_case.dart
@@ -0,0 +1,26 @@
+import 'package:yachaywai/src/core/exceptions/auth_exceptions.dart';
+import 'package:yachaywai/src/domain/entities/usuario.dart';
+import 'package:yachaywai/src/domain/repositories/auth_repository.dart';
+
+class LoginUseCase {
+  final AuthRepository authRepository;
+  LoginUseCase(this.authRepository);
+
+  Future<Usuario?> call({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      // Primero, autenticamos al usuario con sus credenciales
+      await authRepository.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      // Luego, obtenemos la información completa del usuario autenticado
+      return await authRepository.getAuthenticatedUserInfo();
+    } catch (e) {
+      throw UserNotAuthenticatedException();
+    }
+  }
+}
